2f4eeb1f25036bf19f4b36e3b41942cfe650b803
J-A-R-V-I-S\gobre <gobrent21@gmail.com>
2025-02-25 14:23:52 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+<h1 align="center">
+    🌲【CLIENT】🌳
+    <br />
+    🌳 $${\color{#9AA6B2} \text{PoopyDooCrew}}$$ 🌲
+    <br />
+    🌲🌳🌲🌳🌲🌳🌲🌳
+</h1>
+
+### 📖 $${\color{#9AA6B2} \text{OVERVIEW}}$$ 🔍
+
+&nbsp;&nbsp;&nbsp;&nbsp;🔹Each branch corresponds to a specific part of the project. Ensure you're working in the correct branch based on the code you need to modify.
+
+##
+
+### 🌿 $${\color{#9AA6B2} \text{BRANCHES}}$$ 🌿
+
+&nbsp;&nbsp;&nbsp;&nbsp;🔹🪵 [**API**](https://github.com/Brent-W-Anderson/PoopyDooCrew/tree/API) (Backend Development) ➜ This branch contains all backend-related code, including API endpoints, database interactions, and server logic.
+
+&nbsp;&nbsp;&nbsp;&nbsp;🔹🍃 [**CLIENT**](https://github.com/Brent-W-Anderson/PoopyDooCrew/tree/CLIENT) (Frontend Development) ➜ This branch contains all frontend-related code, including UI components, styles, and client-side logic.
---
+++ eslint.config.mjs
@@ -0,0 +1,120 @@
+import js from '@eslint/js'
+import ts from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+
+export default [
+    {
+        ignores: ['dist/', 'node_modules/']
+    },
+    js.configs.recommended,
+    {
+        languageOptions: {
+            parser: tsParser,
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                process: 'readonly',
+                console: 'readonly'
+            }
+        },
+        plugins: {
+            '@typescript-eslint': ts
+        },
+        rules: {
+            ...ts.configs.recommended.rules,
+
+            'no-trailing-spaces': 'error',
+
+            // Alphabetize Class Members & Interface Properties & Imports
+            '@typescript-eslint/member-ordering': [
+                'error',
+                {
+                    default: {
+                        order: 'alphabetically'
+                    }
+                }
+            ],
+
+            // Set maximum line length to enforce automatic breaking
+            'max-len': [
+                'error',
+                {
+                    'code': 80,
+                    'ignoreComments': true,
+                    'ignoreStrings': true,
+                    'ignoreTemplateLiterals': true,
+                    'ignoreRegExpLiterals': true
+                }
+            ],
+
+            // Enforce spacing rules
+            'no-multiple-empty-lines': [
+                'error',
+                {
+                    max: 1,
+                    maxEOF: 1,
+                    maxBOF: 0
+                }
+            ],
+
+            // Force properties to new lines when the object exceeds a certain length
+            'object-curly-newline': [
+                'error',
+                {
+                    ObjectExpression: {
+                        minProperties: 1,
+                        multiline: true,
+                        consistent: true
+                    },
+                    ObjectPattern: {
+                        minProperties: 1,
+                        multiline: true,
+                        consistent: true
+                    },
+                    ImportDeclaration: {
+                        minProperties: 3,
+                        consistent: true
+                    },
+                    ExportDeclaration: {
+                        minProperties: 3,
+                        consistent: true
+                    }
+                }
+            ],
+
+            // parenthesis () formatting: Force properties to be on new lines
+            'function-call-argument-newline': ['error', 'consistent'],
+            'function-paren-newline': ['error', 'multiline'],
+
+            // Object formatting: Force properties to be on new lines
+            'object-property-newline': [
+                'error',
+                {
+                    allowAllPropertiesOnSameLine: false
+                }
+            ],
+
+            // Style rules
+            indent: ['error', 4],
+            semi: ['error', 'never'],
+            quotes: ['error', 'single', {
+                avoidEscape: true
+            }],
+            'space-in-parens': ['error', 'always'],
+            'eol-last': ['error', 'never'],
+            'array-bracket-spacing': ['error', 'never'],
+            'object-curly-spacing': ['error', 'always'],
+            'operator-linebreak': ['error', 'before'],
+            'comma-dangle': ['error', 'never']
+        }
+    },
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+        languageOptions: {
+            parser: tsParser
+        },
+        plugins: {
+            '@typescript-eslint': ts
+        }
+    }
+]
